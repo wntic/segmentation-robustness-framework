59374d5efa910b15225fe2ae5907768c9aa8f72c
Egor Vorobyev <voro6yov.egor@gmail.com>
2024-08-26 14:36:38 +0500

    Add datasets

--- adversarial_segmentation_toolkit/__init__.py
+++ adversarial_segmentation_toolkit/__init__.py
@@ -4,12 +4,20 @@ from .models.deeplab import DeepLabV3
 from .models.fcn import FCN
 from .utils.image_preprocessing import preprocess_image
 from .utils.visualization import show_image, visualize_segmentation
+from .datasets.stanford_background import StanfordBackground
+from .datasets.voc import VOCSegmentation
+from .datasets.ade20k import ADE20K
+from .datasets.cityscapes import Cityscapes
 
 __all__ = [
     "FCN",
     "DeepLabV3",
     "FGSM",
     "PGD",
+    "StanfordBackground",
+    "VOCSegmentation",
+    "ADE20K",
+    "Cityscapes",
     "preprocess_image",
     "visualize_segmentation",
     "show_image",
