f9dc5aec97e737824e5859780caf4dfa914339f1
Egor Vorobyev <voro6yov.egor@gmail.com>
2024-08-26 14:34:43 +0500

    Format with ruff

--- adversarial_segmentation_toolkit/models/__init__.py
+++ adversarial_segmentation_toolkit/models/__init__.py
@@ -1,8 +1,8 @@
 # Base segmentation models
 from .base_model import SegmentationModel
-from .deeplab import DeepLabV3
 
 # Segmentation models
+from .deeplab import DeepLabV3
 from .fcn import FCN
 
-__all__ = ["SegmentationModel", "FCN", "DeepLabV3"]
+__all__ = ["SegmentationModel", "DeepLabV3", "FCN"]
