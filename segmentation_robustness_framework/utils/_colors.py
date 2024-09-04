be6bce4f785e6f24228aa82c3945f3b7391d916c
Egor Vorobyev <voro6yov.egor@gmail.com>
2024-09-04 11:13:01 +0300

    Add classes and colors for VOC dataset
    Added VOC classes and colors to visualize results.

---
+++ segmentation_robustness_framework/utils/_colors.py
@@ -0,0 +1,23 @@
+VOC_colors = [
+    (0, 0, 0),  # Class 0: background
+    (31, 119, 180),  # Class 1: aeroplane
+    (255, 127, 14),  # Class 2: bicycle
+    (44, 160, 44),  # Class 3: bird
+    (214, 39, 40),  # Class 4: boat
+    (148, 103, 189),  # Class 5: bottle
+    (140, 86, 75),  # Class 6: bus
+    (227, 119, 194),  # Class 7: car
+    (127, 127, 127),  # Class 8: cat
+    (188, 189, 34),  # Class 9: chair
+    (23, 190, 207),  # Class 10: cow
+    (174, 199, 232),  # Class 11: diningtable
+    (255, 187, 120),  # Class 12: dog
+    (152, 223, 138),  # Class 13: horse
+    (255, 152, 150),  # Class 14: motorbike
+    (197, 176, 213),  # Class 15: person
+    (196, 156, 148),  # Class 16: potted plant
+    (247, 182, 210),  # Class 17: sheep
+    (199, 199, 199),  # Class 18: sofa
+    (219, 219, 141),  # Class 19: train
+    (158, 218, 229),  # Class 20: tv/monitor
+]
