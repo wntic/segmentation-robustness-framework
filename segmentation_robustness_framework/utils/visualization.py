cc26e330e18dd14199d33c31d4dbb202693e6881
Egor Vorobyev <voro6yov.egor@gmail.com>
2024-09-18 20:53:37 +0300

    Remove show_image function

--- segmentation_robustness_framework/utils/visualization.py
+++ segmentation_robustness_framework/utils/visualization.py
@@ -123,32 +123,3 @@ def visualize_results(
     if save:
         plt.savefig(f"{save_dir}/{len(os.listdir(save_dir))}.jpg", bbox_inches="tight")
     plt.show()
-
-
-def show_image(original_image: Tensor, segmentation_mask: Tensor, normalize=True) -> None:
-    """Displays original image and its corresponding segmentation mask.
-
-    Args:
-        original_image (torch.Tensor): The original image tensor with shape `[1, C, H, W]`.
-        segmentation_mask (torch.Tensor): The segmentation mask tensor with shape `[1, H, W]`.
-
-    Returns:
-        None
-    """
-    if original_image.ndimension() != 4:
-        raise ValueError(f"Expected original image with shape [1, C, H, W], but got {list(original_image.shape)}")
-    if segmentation_mask.ndimension() != 2:
-        raise ValueError(f"Expected segmentation mask with shape [1, H, W], but got {list(segmentation_mask.shape)}")
-
-    image = original_image.squeeze().permute(1, 2, 0).cpu().detach().numpy()
-    if normalize:
-        image = denormalize(image)
-    image = np.clip(image, 0, 1)
-    mask = segmentation_mask.cpu().detach().numpy()
-
-    fig = plt.figure(figsize=(10, 5))
-    fig.add_subplot(1, 2, 1)
-    plt.imshow(image)
-    fig.add_subplot(1, 2, 2)
-    plt.imshow(mask, cmap="tab20")
-    plt.show()
