7f17a680b3d33dcf43fb29f5d4fb61eab114dc55
Egor Vorobyev <voro6yov.egor@gmail.com>
2024-10-29 10:46:07 +0300

    Fix image visualization and saving

--- segmentation_robustness_framework/utils/visualization.py
+++ segmentation_robustness_framework/utils/visualization.py
@@ -174,12 +174,11 @@ def visualize_images(
 
     plt.subplots_adjust(wspace=0.05)
 
-    if show:
-        plt.show()
-
     if save:
         plt.savefig(f"{save_dir}/{len(os.listdir(save_dir))}.jpg", bbox_inches="tight")
-        plt.close()
+    if show:
+        plt.show()
+    plt.close()
 
 
 def visualize_metrics(
