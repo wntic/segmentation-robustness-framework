a4068d382d0f761c2e228cd37e52afb8484b277e
Egor Vorobyev <voro6yov.egor@gmail.com>
2025-07-12 19:12:54 +0500

    Add model loaders to __init__.py

--- segmentation_robustness_framework/loaders/__init__.py
+++ segmentation_robustness_framework/loaders/__init__.py
@@ -1,5 +1,23 @@
 from .attack_loader import AttackLoader
 from .dataset_loader import DatasetLoader
-from .model_loader import ModelLoader
+from .models import (
+    BaseModelLoader,
+    CustomModelLoader,
+    HFSegmentationBundle,
+    HuggingFaceModelLoader,
+    SMPModelLoader,
+    TorchvisionModelLoader,
+    UniversalModelLoader,
+)
 
-__all__ = ["AttackLoader", "DatasetLoader", "ModelLoader"]
+__all__ = [
+    "AttackLoader",
+    "DatasetLoader",
+    "BaseModelLoader",
+    "CustomModelLoader",
+    "HuggingFaceModelLoader",
+    "SMPModelLoader",
+    "TorchvisionModelLoader",
+    "UniversalModelLoader",
+    "HFSegmentationBundle",
+]
