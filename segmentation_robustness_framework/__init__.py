9212ed159f7ede43e66c1e18bc06df2aa641b9d0
Egor Vorobyev <voro6yov.egor@gmail.com>
2025-07-31 16:49:28 +0500

    Reorganize framework structure

--- segmentation_robustness_framework/__init__.py
+++ segmentation_robustness_framework/__init__.py
@@ -1,6 +1,6 @@
 from .__version__ import __version__
 from .attacks import fgsm, pgd, rfgsm, tpgd
-from .config import PipelineConfig
+from .cli import list_components_main, run_config_main, run_tests_main
 from .datasets import ade20k, cityscapes, stanford_background, voc
 from .loaders import attack_loader, dataset_loader
 from .metrics import (
@@ -9,6 +9,7 @@ from .metrics import (
     list_custom_metrics,
     register_custom_metric,
 )
+from .pipeline import PipelineConfig, SegmentationRobustnessPipeline
 from .utils import image_preprocessing, visualization
 
 __all__ = [
@@ -21,11 +22,15 @@ __all__ = [
     "get_custom_metric",
     "image_preprocessing",
     "list_custom_metrics",
+    "list_components_main",
     "MetricsCollection",
     "pgd",
     "PipelineConfig",
     "register_custom_metric",
     "rfgsm",
+    "run_config_main",
+    "run_tests_main",
+    "SegmentationRobustnessPipeline",
     "stanford_background",
     "tpgd",
     "voc",
