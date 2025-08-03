239d4f00de6148daff015eb55f7da1d78dcfd64b
Egor Vorobyev <voro6yov.egor@gmail.com>
2025-08-03 23:35:21 +0500

    Remove example metrics

--- segmentation_robustness_framework/metrics/__init__.py
+++ segmentation_robustness_framework/metrics/__init__.py
@@ -1,12 +1,9 @@
 from .base_metrics import MetricsCollection
 from .custom_metrics import (
     CUSTOM_METRICS_REGISTRY,
-    custom_dice_score,
-    f1_score,
     get_custom_metric,
     list_custom_metrics,
     register_custom_metric,
-    weighted_iou,
 )
 
 __all__ = [
@@ -15,7 +12,4 @@ __all__ = [
     "register_custom_metric",
     "get_custom_metric",
     "list_custom_metrics",
-    "custom_dice_score",
-    "weighted_iou",
-    "f1_score",
 ]
